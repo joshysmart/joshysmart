1eeb851d2e66b60d20a8b3500a6e31426947961b
Joshysmart <sanijoshua10@gmail.com>
2020-07-25 12:17:11 +0100

    Joshysmart

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
 const textArray = ['Joshysmart', 'a web developer ', 'a software engineer'];
 const typingDelay = 200;
 const erasingDelay = 100;
@@ -8,28 +10,32 @@ let charIndex = 0;
 
 function type() {
  if(charIndex < textArray[textArrayIndex].length) {
+  if(!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
   typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
   charIndex++;
   setTimeout(type, typingDelay);
  } else {
   // errase
+  cursorSpan.classList.remove('typing');
   setTimeout(erase, newTextDelay);
  }
 }
 
 function erase() {
  if (charIndex > 0) {
+  if(!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
   typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
   charIndex--;
   setTimeout(erase, erasingDelay);
  } else {
   //type
+  cursorSpan.classList.remove('typing');
   textArrayIndex++;
   if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-  setTimeout(type, typingDelay * 5);
+  setTimeout(type, typingDelay * 6);
  }
 }
 
 window.addEventListener('load', () => {
- setTimeout(type, typingDelay * 2);
+ if (textArray.length) setTimeout(type, typingDelay * 3);
 })

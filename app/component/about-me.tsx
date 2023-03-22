821b118aca2dbaa81a5e58aff534b3c132282abf
Sani Joshua <35628192+joshysmart@users.noreply.github.com>
2023-03-22 11:53:34 +0100

    Update about-me.tsx

--- app/component/about-me.tsx
+++ app/component/about-me.tsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
       <section className="left-section xl:-mb-4">
         <h3 className="text-pureWhite font-bold text-sm md:text-md lg:text-lg  px-4 md:px-8 mb-8 xl:p-0">About Me</h3>
         <p className="text-lightGray  px-4 md:px-8 text-base font-medium md:text-mdp xl:max-w-2xl xl:p-0">
-        My name is Sani Joshua, I am a Junior full-stack developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean Javascript. When writing Javascript I work with React but i can adapt to whatever tools are required. I am based in Nigeria, but i am open to working remotely  and have experience with remote teams. When i am not coding, you’ll find me outdoors. I love being out i nature, listening to music or watching a movie. I’d love you to check out my work.
+        My name is Sani Joshua, I am a Full-stack developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean Javascript. When writing Javascript I work with React but i can adapt to whatever tools are required. I am based in Nigeria, but i am open to working remotely  and have experience with remote teams. When i am not coding, you’ll find me outdoors. I love being out i nature, listening to music or watching a movie. I’d love you to check out my work.
         </p>
       </section>
       <div className="right-section px-4 md:px-8 xl:self-end xl:p-0">
